f548f397e7ad85e4f24558ff0f37f3dcc6f60a62
vishalvasam12 <vishalvasam12>
2019-12-27 18:56:11 +0530

    ADDED:Code for  all email and post code

--- emailProblem.js
+++ emailProblem.js
@@ -1,12 +1,3 @@
 let email=process.argv[2];
 let emailPattern=RegExp('^[0-9a-zA-Z]+([-,_,+,.]{1}[0-9A-Za-z]+){0,1}@[0-9A-Za-z]+.[A-Za-z]{1,3}(.[a-zA-Z]{1,3}){0,1}$');
 let pincheck=emailPattern.test(email);
-if(pincheck==true)
-{
-console.log("TRUe");
-}
-else
-{
-console.log("False");
-}
-
--- pincode.js
+++ pincode.js
@@ -1,11 +1,2 @@
 let regex=RegExp('^4[0-9]{2} [0-9]{3}$');
 let pincheck=regex.test("400 034");
-if(pincheck==true)
-{
-console.log("TRUe");
-}
-else
-{
-console.log("False");
-}
-
