772047da91f5323da5b07033e76de09293404aa7
vishalvasam12 <vishalvasam12>
2019-12-27 18:26:07 +0530

    ADD:Code for checking pincode

--- pincode.js
+++ pincode.js
@@ -1,3 +1,11 @@
-let regex=RegExp('^[4]{1}[0-9]{5}$');
-let pincheck=regex.test("400034");
+let regex=RegExp('^4[0-9]{2} [0-9]{3}$');
+let pincheck=regex.test("400 034");
+if(pincheck==true)
+{
+console.log("TRUe");
+}
+else
+{
+console.log("False");
+}
 
